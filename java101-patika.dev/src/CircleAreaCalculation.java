3cfc8941c4485497197551a81ee9102ed0a4ca48
Zeynep Elverisli <109090494+ezeynepel@users.noreply.github.com>
2023-01-17 17:36:51 +0300

    Add files via upload

---
+++ java101-patika.dev/src/CircleAreaCalculation.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class CircleAreaCalculation {
+
+	public static void main(String[] args) {
+		double pi=3.14, area, perimeter,a;
+		int r;
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Dairenin yarı çapını giriniz: ");
+		r =sc.nextInt();
+		
+		System.out.println("Merkez açısının ölçüsünü giriniz: ");
+		a =sc.nextDouble();
+		
+		
+		area = (pi*r*r*a)/360;
+		perimeter = 2* pi*r;
+		
+		System.out.println("Dairenin alanı: "+ area);
+		System.out.printf("Dairenin çevresi: %.2f", perimeter);
+		
+		
+		
+		
+	}
+
+}
