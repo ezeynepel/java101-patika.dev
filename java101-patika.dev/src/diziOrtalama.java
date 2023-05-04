e6a402dc5c47dbe765cb40125ee4689e0bc27b6d
Elif Zeynep Elverisli <109090494+ezeynepel@users.noreply.github.com>
2023-05-04 12:31:41 +0300

    Add files via upload

---
+++ java101-patika.dev/src/diziOrtalama.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class diziOrtalama {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n;
+		double sum = 0.0, har = 0.0;
+
+		System.out.println("Tavan sayıyı giriniz: ");
+		n = sc.nextInt();
+
+		double[] numbers = new double[n];
+
+		for (double i = 0; i < n; i++) {
+			numbers[(int) i] = 1 / (i + 1);
+			sum += numbers[(int) i];
+
+		}
+		System.out.println("Dizideki sayıların ortalaması: " + sum / numbers.length);
+		har = n / sum;
+		System.out.println("Dizideki sayıların harmonik ortalaması: " + har);
+
+	}
+
+}
